f0bee2680e5bff533ea096d2decff1272c600b2a
rupel <23055682+rupel190@users.noreply.github.com>
2025-01-11 19:57:05 +0100

    finish downloader + ignore

--- bulk-downloader.js
+++ bulk-downloader.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Humble Downloader w/ ignore list - for downloads at GameDevMarket
+// @name        Humble Downloader w/o ignore list - for downloads at GameDevMarket
 // @namespace   Violentmonkey Scripts
 // @match       https://www.gamedevmarket.net/user/external/purchases
 // @grant       none
@@ -10,55 +10,41 @@
 
 console.log("HELLO GAMEDEVMONKEY!");
 
-var _buttons = [ ]
-var _links = [ ]
+
+const assetContainers = document.querySelectorAll('.flex.flex-col.md\\:flex-row.w-full.border-1.border-gray-300.my-2.px-4.py-2');
 const _ignore = `
   `;
 
 
 async function download() {
     console.log("Starting to download ...");
+    ignored_files = _ignore.trim().split('\n').map(file => file.trim()); // Trim whole file and each filename
 
-    while (_links.length > 0) {
-      let link = _links.pop();
-      const filename = link.href.split('/').pop().split('?')[0];
-      ignored_files = _ignore.trim().split('\n').map(file => file.trim()); // Trim whole file and each filename
+      for(const container of assetContainers) {
+        button = container.querySelector('input[name="submit"][value="Download"]');
+        filename = container.querySelector('a.dark\\:hover\\:text-primary-teal').href.split('/').pop();
+        console.log(filename, " ", button);
 
-      if(!ignored_files.includes(filename)) {
-        console.log("Attempt download ", filename, " ", link.href);
-        window.open(link.href, "_blank");
-        await new Promise(resolve => setTimeout(resolve, 30000)); // Delay between downloads
+        if(!ignored_files.includes(filename)) {
+          button.click();
+          await new Promise(resolve => setTimeout(resolve, 30000)); // Delay between downloads
       } else {
-        console.log("IGNORE ", filename);
+        console.log("Ignore ", filename);
       }
-      console.log("Remaining: ", _links.map(link => link.href));
     }
 }
 
-const downloadButton = document.querySelector('input[name="submit"][value="Download"]');
-
 const observer = new MutationObserver(() => {
-    if(_buttons.length <= 0) {
-      
-      downloadButton.click();
-
-      _buttons = _buttons.forEach(link => {
-          if (link) {
-              _links.push(link)
-              // Highlight link
-              link.style.backgroundColor = 'yellow';
-          } else {
-              console.log('No link found.');
-          }
-      });
+    if(assetContainers.length > 0) {
+        console.log("Found download buttons", assetContainers);
+        observer.disconnect();
+        download();
     } else {
-      console.log("Found download links", _links);
-      observer.disconnect();
-      download();
+      console.log('No downloads found yet.');
     }
 });
 
 // Start observing changes to the DOM
 observer.observe(document.body, { childList: true, subtree: true });
 
- 
+
