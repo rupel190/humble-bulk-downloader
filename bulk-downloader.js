22999406a88bd0829cdfbbd1296d607c7d02bc07
rupel <23055682+rupel190@users.noreply.github.com>
2025-01-12 14:03:38 +0100

    Simplified to regular download similar to humble direct downloader

--- bulk-downloader.js
+++ bulk-downloader.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Humble Downloader w/o ignore list - for downloads at GameDevMarket
+// @name        Humble Downloader w/ ignore list - for downloads at GameDevMarket
 // @namespace   Violentmonkey Scripts
 // @match       https://www.gamedevmarket.net/user/external/purchases
 // @grant       none
@@ -11,32 +11,38 @@
 console.log("HELLO GAMEDEVMONKEY!");
 
 
-const assetContainers = document.querySelectorAll('.flex.flex-col.md\\:flex-row.w-full.border-1.border-gray-300.my-2.px-4.py-2');
+const _assetContainers = document.querySelectorAll('.flex.flex-col.md\\:flex-row.w-full.border-1.border-gray-300.my-2.px-4.py-2');
 const _ignore = `
   `;
+let _links = []
 
 
 async function download() {
     console.log("Starting to download ...");
     ignored_files = _ignore.trim().split('\n').map(file => file.trim()); // Trim whole file and each filename
 
-      for(const container of assetContainers) {
+      for(const container of _assetContainers) {
+        console.log('Container: ', container)
         button = container.querySelector('input[name="submit"][value="Download"]');
         filename = container.querySelector('a.dark\\:hover\\:text-primary-teal').href.split('/').pop();
-        console.log(filename, " ", button);
 
+        const form = container.querySelector('form[action="/user/products/download"]');
+        const formData = new FormData(form);
+        // await getFormUrl(container);
         if(!ignored_files.includes(filename)) {
           button.click();
-          await new Promise(resolve => setTimeout(resolve, 30000)); // Delay between downloads
+          await new Promise(resolve => setTimeout(resolve, 5000)); // Delay between downloads
       } else {
         console.log("Ignore ", filename);
       }
     }
+
+    downloadTxt();
 }
 
 const observer = new MutationObserver(() => {
-    if(assetContainers.length > 0) {
-        console.log("Found download buttons", assetContainers);
+    if(_assetContainers.length > 0) {
+        console.log("Found downloads", _assetContainers);
         observer.disconnect();
         download();
     } else {
